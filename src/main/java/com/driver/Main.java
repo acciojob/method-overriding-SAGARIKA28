a45c8605ba59cfb7c874134c5e840bc65cd916c2
sagarika28 <saga28r@gmail.com>
2022-12-10 18:34:07 +0530

    Method overriding assignment.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,19 +2,18 @@ package com.driver;
 
 public class Main{
     public static class A{
-    String meth(){
-      return "Invoking method from class A";
+        String meth(){
+            return "Invoking method from class A";
+        }
     }
-}
     public static class B extends A{
     @Override
-    public String meth(){
-       return "Method is overridden in Extendend class B";
-    }
+        public String meth(){
+           return "Method is overridden in Extendend class B";
+        }
     }
     public static void main(String[] args){
         B obj = new B();
-        System.out.print(obj.meth());
-        
+        System.out.print(obj.meth()); 
     }
 }
